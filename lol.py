1fb084ff3917d77753167abd2a27dfb04825588c
Kaustubh <kaustubh@fab.com>
2014-07-26 11:05:35 +0530

    Generating random string.

--- lol.py
+++ lol.py
@@ -1,2 +1,24 @@
-def open_dev_random
-    open('/dev/random')
+import sys
+import string
+import random
+from time import gmtime, strftime
+
+def open_dev_random(file):
+    # print file.read()
+    # print os.urandom
+
+    # for i in xrange(100000):
+    #     sys.stdout.write(random.choice(string.letters))
+
+    while True:
+      # print '1'
+      for i in xrange(100):
+        sys.stdout.write(random.choice(string.letters))
+      # print '\n'  
+      print strftime("%Y-%m-%d %H:%M:%S", gmtime())  
+      print 'Generating'
+
+dev_random = open('/dev/urandom')
+
+open_dev_random(dev_random)
+
