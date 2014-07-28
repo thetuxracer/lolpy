c0ad3004780537a37aa4c565ca1790f784f4ce28
Kaustubh <kaustubh@fab.com>
2014-07-28 12:44:00 +0530

    One liner, added random sleeper. Might need a dictionary.

--- lol.py
+++ lol.py
@@ -1,24 +1,30 @@
 import sys
 import string
 import random
-from time import gmtime, strftime
+from time import time, sleep, gmtime, strftime
+from random import randrange
 
-def open_dev_random(file):
+def open_dev_random():
     # print file.read()
     # print os.urandom
 
     # for i in xrange(100000):
     #     sys.stdout.write(random.choice(string.letters))
+    foo = ''
 
     while True:
       # print '1'
       for i in xrange(100):
-        sys.stdout.write(random.choice(string.letters))
+        foo += random.choice(string.letters)
       # print '\n'  
-      print strftime("%Y-%m-%d %H:%M:%S", gmtime())  
-      print 'Generating'
+      current_date = strftime("%Y-%m-%d %H:%M:%S", gmtime())  
+      # print 'Generating'
+      print "[%s] Generating %s" % (current_date, foo)
+      foo = ''
+      sleep(randrange(0, 3))
 
-dev_random = open('/dev/urandom')
+ 
+# dev_random = open('/dev/urandom')
 
-open_dev_random(dev_random)
+open_dev_random()
 
